fdd6c274030a9f837269b8fcb6c8f6c807a337fc
David Newkerk <keysplayer@gmail.com>
2014-09-19 03:54:10 -0700

    Add ability to edit and update users

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
+  sequence(:email) { |n| "user#{n}@example.com" }
+
   factory :user do
     name 'Example User'
-    email 'user@example.com'
+    email { generate(:email) }
     password 'example1234'
     password_confirmation 'example1234'
 
--- spec/features/admin/creating_users_spec.rb
+++ spec/features/admin/creating_users_spec.rb
@@ -21,7 +21,7 @@ feature 'Creating Users' do
   scenario 'Creating an admin user' do
     fill_in 'Email', with: 'admin@example.com'
     fill_in 'Password', with: 'password'
-    check 'Is this an admin?'
+    check 'Is an admin?'
     click_button 'Create User'
     expect(page).to have_content('User has been created.')
     within('#users') do
